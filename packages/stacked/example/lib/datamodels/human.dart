d3932f595b15cd9f17a1dfbc35328e6cfe70ac00
Wondergrade LLC <john@wondergrade.com>
2021-05-15 15:17:32 -0700

    removed example, updated pubspec versions

--- packages/stacked/example/lib/datamodels/human.dart
+++
@@ -1,6 +0,0 @@
-class Human {
-  final String? name;
-  final String? surname;
-
-  Human({this.name, this.surname});
-}
--- packages/stacked/example/lib/ui/bottom_nav/history/history_viewmodel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:stacked/stacked.dart';
-
-class HistoryViewModel extends FutureViewModel<int> {
-  @override
-  Future<int> futureToRun() async {
-    await Future.delayed(Duration(seconds: 2));
-    return 100;
-  }
-}
